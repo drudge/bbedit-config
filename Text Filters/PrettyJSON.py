98a9622f03fd2376f22b8949168f66f97ba242bc
Nicholas Penree <nick@weborate.com>
2012-02-21 22:38:31 -0500

    Fix errors in Pretty JSON script

--- Text Filters/PrettyJSON.py
+++ Text Filters/PrettyJSON.py
@@ -1,10 +1,15 @@
 #!/usr/local/bin/python
 import fileinput
 import json
-if __name__ == "__main__":
-  jsonStr = ''
-  for a_line in fileinput.input():
-    jsonStr = jsonStr + ' ' + a_line.strip()    
-  jsonObj = json.loads(jsonStr)  
-  print json.dumps(jsonObj, sort_keys=True, indent=2) 
 
+if __name__ == "__main__":
+	jsonStr = ''
+
+	for a_line in fileinput.input():
+		jsonStr = jsonStr + a_line
+
+	try:
+		jsonObj = json.loads(jsonStr)
+		print json.dumps(jsonObj, sort_keys=True, indent=2)
+	except:
+		print jsonStr.strip()
